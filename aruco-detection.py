c0467ed568793e896709d259686da63256594b56
Angelo <angelodavid98@hotmail.com>
2022-03-28 02:02:45 -0600

    New camera calibration added

--- aruco-detection.py
+++ aruco-detection.py
@@ -4,9 +4,7 @@ import numpy as np
 import imutils
 
 MARKER_SIZE = 100 # milimeters
-CAMERA_WIDTH = 320
-CAMERA_HEIGHT = 240
-FPS = 30
+BLUE = (255, 0, 0)
 
 with np.load('camera_matrix.npz') as X:
 	K, dist, rvecs, tvecs = [X[i] for i in ('camera_matrix',
@@ -20,25 +18,24 @@ ARUCO_DICT = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
 # Define camera to use and set resolution and frame rate
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
-cap.set(2, CAMERA_WIDTH)
-cap.set(4, CAMERA_HEIGHT)
-cap.set(5, FPS)
 
 while True:
 	# Read frames of the camera
-	ret, img = cap.read()
+	ret, frame = cap.read()
 
 	# Convert image to gray scale
-	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	corners, ids, rejected = aruco.detectMarkers(img_gray, 
+	corners, ids, rejected = aruco.detectMarkers(frame_gray, 
 												 ARUCO_DICT,
 												 K,
 												 dist)
 
+	cv2.circle(frame, (287, 231), 5, BLUE, -1)
+
 	# Verify at least one ArUco marker was detected
 	if len(corners) > 0 or ids is not None:
-		aruco.drawDetectedMarkers(img, corners)
+		aruco.drawDetectedMarkers(frame, corners)
 
 		# Get pose of every single marker
 		rvec_list_all, tvec_list_all, obj_points = aruco.estimatePoseSingleMarkers(corners, MARKER_SIZE, K, dist)
@@ -46,13 +43,26 @@ while True:
 		rvec = rvec_list_all[0][0]
 		tvec = tvec_list_all[0][0]
 
-		# Draw axis and write ids on all markers
+		# Draw axis and corners of the markers
 		for marker in range(len(ids)):
-			aruco.drawAxis(img, K, dist, rvec, tvec, 100)
+			for corner in range(4):
+				corner_x, corner_y = corners[marker][0, corner]
+				center_coordinates = tuple((int(corner_x), int(corner_y)))
+				cv2.circle(frame, center_coordinates, 5, BLUE, -1)
+
+			aruco.drawAxis(frame, K, dist, rvec_list_all[marker], tvec_list_all[marker], 50)
+
+		tvec_str_x = 'x = {0:4.0f}'.format(tvec[0])
+		tvec_str_y = 'y = {0:4.0f}'.format(tvec[1])
+		tvec_str_z = 'z = {0:4.0f}'.format(tvec[2])
+		cv2.putText(frame, tvec_str_x, (50, 50), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+		cv2.putText(frame, tvec_str_y, (50, 65), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+		cv2.putText(frame, tvec_str_z, (50, 80), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
 
 
-	cv2.imshow('Aruco Detection - RGB', img)
-	cv2.imshow('Aruco Detection - Gray', img_gray)
+	cv2.imshow('Aruco Detection - RGB', frame)
+	cv2.imshow('Aruco Detection - Gray', frame_gray)
+
 
 	# If ESC pressed exit
 	if cv2.waitKey(1) & 0xFF == 27:
